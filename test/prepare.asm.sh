f502c63c1f5caa805d8dca2be437d0ff0416be6b
Zepu Miao <30388546+codeatcg@users.noreply.github.com>
2024-11-02 19:31:35 +0800

    Add files via upload

---
+++ test/prepare.asm.sh
@@ -0,0 +1,7 @@
+# primary reference -- GCF_000146045.2_R64_genomic.fna.gz
+dataDir=`pwd`
+echo "SGDref#HP0 $dataDir/GCF_000146045.2_R64_genomic.fna.gz" >> build.asm.txt
+echo "DBVPG6765#HP0 $dataDir/DBVPG6765.genome.fa.gz" >> build.asm.txt
+echo "Y12#HP0 $dataDir/Y12.genome.fa.gz" >> build.asm.txt
+echo "SK1#HP0 $dataDir/SK1.genome.fa.gz" >> build.asm.txt
+echo "DBVPG6044#HP0 $dataDir/DBVPG6044.genome.fa.gz" >> build.asm.txt
---
+++ test/prepare.gff.sh
@@ -0,0 +1,7 @@
+dataDir=`pwd`
+echo "SGDref#HP0 $dataDir/GCF_000146045.2_R64_genomic.gff.gz NA" >> build.gff.txt
+echo "DBVPG6765#HP0 $dataDir/DBVPG6765.all_feature.gff.gz NA" >> build.gff.txt
+echo "Y12#HP0 $dataDir/Y12.all_feature.gff.gz NA" >> build.gff.txt
+echo "SK1#HP0 $dataDir/SK1.all_feature.gff.gz NA" >> build.gff.txt
+echo "DBVPG6044#HP0 $dataDir/DBVPG6044.all_feature.gff.gz NA" >> build.gff.txt
+
