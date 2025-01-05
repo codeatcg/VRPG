3b86f69c32a3d37167f9c0330b0a036fa98eb217
Zepu Miao <30388546+codeatcg@users.noreply.github.com>
2025-01-05 21:28:14 +0800

    Add files via upload

---
+++ test/download.testData.sh
@@ -0,0 +1,15 @@
+wget -c https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.fna.gz
+wget -c https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.gff.gz
+
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_Genome/SK1.genome.fa.gz
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/SK1.all_feature.gff.gz
+
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_Genome/Y12.genome.fa.gz
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/Y12.all_feature.gff.gz
+
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_Genome/DBVPG6765.genome.fa.gz
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/DBVPG6765.all_feature.gff.gz
+
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_Genome/DBVPG6044.genome.fa.gz
+wget -c http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/DBVPG6044.all_feature.gff.gz
+
